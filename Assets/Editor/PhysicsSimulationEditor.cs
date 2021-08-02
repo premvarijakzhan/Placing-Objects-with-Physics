2b0bc8d523783914157faafe7186659dba14f657
Your Name <55784578+premvarijakzhan@users.noreply.github.com>
2021-08-02 21:24:39 +0800

    Added the project build.

---
+++ Assets/Editor/PhysicsSimulationEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PhysicsSimulation))]
+public class PhysicsSimulationEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PhysicsSimulation sim = (PhysicsSimulation)target;
+
+        if (GUILayout.Button("Simulate"))
+        {
+            sim.RunSimulation();
+        }
+
+        if (GUILayout.Button("Reset"))
+        {
+            sim.ResetAllBodies();
+        }
+    }
+
+}
